7ddae092b18f4c43732ee315c3fa0a7efc905940
Amir Khosravi <amirkhosravi921@gmail.com>
2024-07-18 17:18:41 +0330

    Form submission + list of tasks added

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,44 @@ import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import { IoAdd, IoCloseOutline } from "react-icons/io5";
+import { z } from "zod";
+
+export const schema = z.object({
+  title: z
+    .string()
+    .min(3, { message: "Title should be more than 3 characters" }),
+  time: z
+    .number({ invalid_type_error: "Time is required." })
+    .positive({ message: "Please enter a valid time for your task" }),
+});
+
+export type TodoFormData = z.infer<typeof schema>;
 
 function App() {
   const [isOpenForm, setOpenForm] = useState(false);
+  const [todoTask, setTodoTask] = useState<TodoFormData[]>([]);
+
   return (
     <>
       <button onClick={() => setOpenForm(!isOpenForm)}>
         {!isOpenForm ? <IoAdd /> : <IoCloseOutline />}
       </button>
-      {isOpenForm && <Form />}
+      {isOpenForm && (
+        <Form
+          onSubmit={(todo) =>
+            setTodoTask([
+              ...todoTask,
+              { ...todo, title: todo.title, time: todo.time },
+            ])
+          }
+        />
+      )}
+
+      <ul>
+        {todoTask.map((todo, index) => (
+          <li key={index}>{todo.title}</li>
+        ))}
+      </ul>
     </>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,32 @@
-const Form = () => {
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { schema, TodoFormData } from "../App";
+
+interface Props {
+  onSubmit: (data: TodoFormData) => void;
+}
+
+const Form = ({ onSubmit }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<TodoFormData>({ resolver: zodResolver(schema) });
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="title">Title:</label>
-        <input name="title" type="text" />
+        <input {...register("title")} name="title" type="text" />
+        {errors.title && <p>{errors.title.message}</p>}
         <label htmlFor="time">Time</label>
-        <input name="time" type="number" />
+        <input
+          {...register("time", { valueAsNumber: true })}
+          name="time"
+          type="number"
+        />
+        {errors.time && <p>{errors.time.message}</p>}
         <button type="submit">Submit</button>
       </form>
     </div>
